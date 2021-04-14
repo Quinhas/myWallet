88ea6ba5ec7735499bb428bc4c68801a78e4bd69
Quinhas <ln.santana08@gmail.com>
2021-04-14 18:04:29 -0300

    Primeiras configurações

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Layout } from './components';
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles/>
+      <Layout/>
+    </>
+  );
+}
+
+export default App;
