8880aeed9e0e18c49e3809ad82748f1330be4fad
Quinhas <ln.santana08@gmail.com>
2021-04-15 17:58:54 -0300

    Rotas

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,12 @@
+import { Route, Switch } from 'react-router-dom';
+
+import { Dashboard, List } from '../pages';
+
+const AppRoutes: React.FC = () => (
+  <Switch>
+    <Route path="/dashboard" exact component={Dashboard}></Route>
+    <Route path="/list/:type" component={List}></Route>
+  </Switch>
+);
+
+export default AppRoutes;
